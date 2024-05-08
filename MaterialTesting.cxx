e0ed0fee41fcdfe7f89670c2542eab35a170085e
Kristian Zarębski <krizar312@gmail.com>
2024-05-08 19:58:22 +0000

    Use path to macro in code

--- MaterialTesting.cxx
+++ MaterialTesting.cxx
@@ -19,12 +19,13 @@ int main(int argc, char** argv) {
         "This simulation models a particle gun positioned along the Z axis relative to a target of Beryllium.\n\n"
     );
     args::HelpFlag help(parser, "help", "Display this help menu", {'h', "help"});
-    args::ValueFlag<std::filesystem::path> file_flag(parser, "config_file", "Read inputs from configuration file", {'f'});
-    args::ValueFlag<G4String> phys_list_flag(parser, "physics_list", "Use specificed G4 Physics list", {'l'});
-    args::ValueFlag<G4String> root_out_flag(parser, "output_file", "ROOT file to write results to", {'o'});
-    args::ValueFlag<G4double> momentum_flag(parser, "momentum", "Momentum Z component in GeV", {'m'});
-    args::ValueFlag<G4double> target_dz_flag(parser, "target_thickness", "Size of target in the Z axis in metres", {'t'});
-    args::ValueFlag<G4double> gun_z_offset_flag(parser, "gun_z_offset", "Z axis position of particle fun in metres", {'z'});
+    args::Flag batch_mode(parser, "batch_mode", "Run in batch mode, no UI", {'b', "batch"});
+    args::ValueFlag<std::filesystem::path> file_flag(parser, "config_file", "Read inputs from configuration file", {'c', "config"});
+    args::ValueFlag<G4String> phys_list_flag(parser, "physics_list", "Use specificed G4 Physics list", {'l', "physics-list"});
+    args::ValueFlag<G4String> root_out_flag(parser, "output_file", "ROOT file to write results to", {'o', "output"});
+    args::ValueFlag<G4double> momentum_flag(parser, "momentum", "Momentum Z component in GeV", {'m', "momentum"});
+    args::ValueFlag<G4double> target_dz_flag(parser, "target_thickness", "Size of target in the Z axis in metres", {'t', "target-thickness"});
+    args::ValueFlag<G4double> gun_z_offset_flag(parser, "gun_z_offset", "Z axis position of particle gun in metres", {'z', "pgun-zpos"});
 
     try
     {
@@ -48,6 +49,11 @@ int main(int argc, char** argv) {
         return 1;
     }
 
+    std::filesystem::path macro_path = std::filesystem::path(argv[0]).parent_path() / "vis.mac";
+
+    if(!std::filesystem::exists(macro_path)) {
+        throw std::runtime_error("Expected macro file '" + macro_path.string() + "' but file does not exist");
+    }
 
     CLHEP::HepRandom::setTheEngine(new CLHEP::RanecuEngine());
     G4long seed = time(NULL);
@@ -111,17 +117,24 @@ int main(int argc, char** argv) {
         )
     );
     run_manager->Initialize();
+    G4UIExecutive* ui = nullptr;
 
-    G4UIExecutive* ui = new G4UIExecutive(argc, argv);
-
-    G4VisManager* vis_manager = new G4VisExecutive;
-    vis_manager->Initialize();
+    if(!batch_mode) {
+        ui = new G4UIExecutive(argc, argv);
+        G4VisManager* vis_manager = new G4VisExecutive;
+        vis_manager->Initialize();
+    }
 
     G4UImanager* ui_manager = G4UImanager::GetUIpointer();
 
-    ui_manager->ApplyCommand("/control/execute vis.mac");
-
-    ui->SessionStart();
+    if(!batch_mode) {
+        G4cout << "Opening "<< macro_path.string() << G4endl;
+        ui_manager->ApplyCommand("/control/execute " + macro_path.string());
+        ui->SessionStart();
+    }
+    else {
+        ui_manager->ApplyCommand("/run/beamOn");
+    }
 
     return 0;
 }
